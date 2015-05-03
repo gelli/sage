23a0e3995e60ed7180c97bb733a5a40959941fd4
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-05-03 18:33:57 +0200

    Fix indentation

--- templates/front-page/section-two.php
+++ templates/front-page/section-two.php
@@ -5,21 +5,21 @@ use Roots\Sage\Assets;
 <div class="row">
   <div class="col-md-4">
     <h2 class="featured-posts">Unser Vlog</h2>
-        <div class="teaser-box videoWrapper">
-            <iframe src="//www.youtube.com/embed/?list=PLvr0Ef_Rv_qkBq1YZjyF2nLdWyAkwQk7L" frameborder="0" allowfullscreen></iframe>
-        </div>
+    <div class="teaser-box videoWrapper">
+      <iframe src="//www.youtube.com/embed/?list=PLvr0Ef_Rv_qkBq1YZjyF2nLdWyAkwQk7L" frameborder="0" allowfullscreen></iframe>
+    </div>
   </div>
   <div class="col-md-4">
     <h2 class="featured-posts">Urlaubsfilme</h2>
-        <div class="teaser-box videoWrapper">
-            <iframe src="//www.youtube.com/embed/?list=PLvr0Ef_Rv_qmXMc-Qrq4eChzOJuYPW6FC" frameborder="0" allowfullscreen></iframe>
-        </div>
+    <div class="teaser-box videoWrapper">
+      <iframe src="//www.youtube.com/embed/?list=PLvr0Ef_Rv_qmXMc-Qrq4eChzOJuYPW6FC" frameborder="0" allowfullscreen></iframe>
+    </div>
   </div>
   <div class="col-md-4">
     <h2 class="featured-posts">Lerne fotografieren</h2>
-         <a href="https://my.leadpages.net/leadbox/140f32873f72a2%3A13706bf69b46dc/5632908932939776/">
-            <img class="img-responsive" src="<?php echo Assets\asset_path('images/ebook2.jpg');?>" border="1"/>
-          </a>
-          <script data-leadbox="140f32873f72a2:13706bf69b46dc" data-url="https://my.leadpages.net/leadbox/140f32873f72a2%3A13706bf69b46dc/5632908932939776/" data-config="%7B%7D" type="text/javascript" src="//my.leadpages.net/leadbox-901.js"></script>
+    <a href="https://my.leadpages.net/leadbox/140f32873f72a2%3A13706bf69b46dc/5632908932939776/">
+      <img class="img-responsive" src="<?php echo Assets\asset_path('images/ebook2.jpg');?>" border="1"/>
+    </a>
+    <script data-leadbox="140f32873f72a2:13706bf69b46dc" data-url="https://my.leadpages.net/leadbox/140f32873f72a2%3A13706bf69b46dc/5632908932939776/" data-config="%7B%7D" type="text/javascript" src="//my.leadpages.net/leadbox-901.js"></script>
   </div>
- </div>
+</div>
