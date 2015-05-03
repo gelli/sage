06ee5870b5fd4965fc7cbac0ccf4f8996d58c824
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-05-03 20:08:17 +0200

    Don't use bloginfo for search placeholder

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" action="<?php  esc_url( home_url( '/' ) ) ?>">
   <div class="input-group">    
-    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search', 'sage'); ?> <?php bloginfo('name'); ?>" required>
+    <input type="search" value="<?= get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Durchsuche Little Blue Bag', 'sage'); ?>" required>
     <span class="input-group-btn">
       <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
     </span>
