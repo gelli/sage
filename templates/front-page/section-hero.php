04c6afaf1c514bd0149262ed54de98a8c986fe60
Christian Gellweiler <christian.gellweiler@1und1.de>
2015-12-09 23:09:58 +0100

    link to ebook page instead of opening newsletter popup

--- templates/front-page/section-hero.php
+++ templates/front-page/section-hero.php
@@ -16,7 +16,7 @@ use Roots\Sage\Assets;
   <div class="hero-splash">
     <div class="hero-splash-text">
       <img class="img-responsive" src="<?php echo Assets\asset_path('images/sticker.png');?>">
-      <a class="btn btn-action btn-lg" data-leadbox="140f32873f72a2:13706bf69b46dc" href="https://my.leadpages.net/leadbox/140f32873f72a2%3A13706bf69b46dc/5632908932939776/">
+      <a class="btn btn-action btn-lg" href="/ebook-manuell-fotografieren/">
         Lerne fotografieren
       </a>
     </div>
