7fe830c9cbb45edda6e3b82145c124af289e2efc
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-04-06 22:18:30 +0200

    Disable sensei settings if sensei plugin is not active

--- lib/sensei.php
+++ lib/sensei.php
@@ -7,10 +7,13 @@
  */
 
 global $woothemes_sensei;
-remove_action( 'sensei_before_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper' ), 10 );
-remove_action( 'sensei_after_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper_end' ), 10 );
-remove_action( 'sensei_comments', array( $woothemes_sensei->frontend, 'sensei_output_comments' ), 10 );
 
-add_action( 'after_setup_theme', function () {
-  add_theme_support( 'sensei' );
-});
+if (isset($woothemes_sensei)) {
+  remove_action( 'sensei_before_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper' ), 10 );
+  remove_action( 'sensei_after_main_content', array( $woothemes_sensei->frontend, 'sensei_output_content_wrapper_end' ), 10 );
+  remove_action( 'sensei_comments', array( $woothemes_sensei->frontend, 'sensei_output_comments' ), 10 );
+
+  add_action( 'after_setup_theme', function () {
+    add_theme_support( 'sensei' );
+  });
+}
