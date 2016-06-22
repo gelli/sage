ef0d0db3796770698baf82d0bc8c97403039b232
Christian Gellweiler <christian.gellweiler@1und1.de>
2016-06-22 20:46:32 +0200

    Add link to vlog playlist on youtube

--- templates/front-page/section-two.php
+++ templates/front-page/section-two.php
@@ -5,7 +5,7 @@ use Roots\Sage\Assets;
 <div class="row">
   <div class="col-md-4">
     <h2 class="featured-posts featured-travel">
-      <a href="#">Unser Vlog</a>
+      <a href="//www.youtube.com/watch?list=PLvr0Ef_Rv_qmQFCXTYtsT-8lSXSXVnKec">Unser Vlog</a>
     </h2>
     <div class="teaser-box videoWrapper">
       <iframe src="//www.youtube.com/embed/?list=PLvr0Ef_Rv_qmQFCXTYtsT-8lSXSXVnKec" frameborder="0" allowfullscreen></iframe>
