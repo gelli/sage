d4edae4514434ffd6dfc0c7d67546fac6394d2be
Christian Gellweiler <cgellweiler@gmail.com>
2017-01-14 17:57:54 +0100

    Implement custom template

--- template-custom.php
+++ template-custom.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: Custom Template (no sidebar)
  */
-?>
 
+  while (have_posts()) : the_post();
+    get_template_part('templates/content', 'page');
+  endwhile;
+?>
