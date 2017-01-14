098e415b56d9d464e0e6e527a053fc1749de848d
Christian Gellweiler <cgellweiler@gmail.com>
2017-01-14 20:34:36 +0100

    fix indenting

--- templates/header.php
+++ templates/header.php
@@ -11,23 +11,22 @@ if (is_front_page()) { ?>
   <?php } ?>
 
   <div class="main-header">
-      <?php get_template_part('templates/header', 'navigation'); ?>
-        <div class="header-content container text-center ">
-          <div class="sticker">
-            <img class="img-responsive" src="<?php echo Assets\asset_path('images/sticker.png');?>"/>
-          </div>
-
-          <div class="row jumbotron">
-              <h2>Legendäre Reisefotos, das kannst du auch!</h2>
-              <h2>Lerne mit uns fotografieren.</h2>
-          </div>
-          <div class="row">
-            <div class="col-xs-12 text-center">
-              <a href="/ebook-manuell-fotografieren" class="btn-hero btn btn-lg">kostenloses eBook</a>
-              <a href="/produkt/littlebluebag-fotokurs-lerne-online-fotografieren" class="btn-hero btn btn-lg">Der Fotokurs</a>
-            </div>
-          </div>
+    <?php get_template_part('templates/header', 'navigation'); ?>
+    <div class="header-content container text-center ">
+      <div class="sticker">
+        <img class="img-responsive" src="<?php echo Assets\asset_path('images/sticker.png');?>"/>
+      </div>
+      <div class="row jumbotron">
+        <h2>Legendäre Reisefotos, das kannst du auch!</h2>
+        <h2>Lerne mit uns fotografieren.</h2>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 text-center">
+          <a href="/ebook-manuell-fotografieren" class="btn-hero btn btn-lg">kostenloses eBook</a>
+          <a href="/produkt/littlebluebag-fotokurs-lerne-online-fotografieren" class="btn-hero btn btn-lg">Der Fotokurs</a>
         </div>
+      </div>
+    </div>
   </div>
 <?php } else {
   get_template_part('templates/header', 'navigation');
