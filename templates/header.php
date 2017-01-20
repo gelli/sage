f5065f5308b62fdc26a117099b87a081a94e5e8e
Christian Gellweiler <cgellweiler@gmail.com>
2017-01-20 07:45:03 +0100

    Change workshop link target

--- templates/header.php
+++ templates/header.php
@@ -23,7 +23,7 @@ if (is_front_page()) { ?>
       <div class="row">
         <div class="col-xs-12 text-center">
           <a href="/ebook-manuell-fotografieren" class="btn-hero btn btn-lg">kostenloses eBook</a>
-          <a href="/produkt/littlebluebag-fotokurs-lerne-online-fotografieren" class="btn-hero btn btn-lg">Der Fotokurs</a>
+          <a href="/workshop/" class="btn-hero btn btn-lg">Der Fotokurs</a>
         </div>
       </div>
     </div>
