00076c8307aa084b37f5540d4bf1d2e120b5a96e
Christian Gellweiler <cgellweiler@gmail.com>
2017-01-23 22:25:58 +0100

    Make sure tinymce exists

--- assets/scripts/tinymce_hintbox.js
+++ assets/scripts/tinymce_hintbox.js
@@ -1,23 +1,25 @@
 (function() {
-  tinymce.create('tinymce.plugins.StonyRayHintBox', {
-      init : function(ed, url) {
-          ed.addButton('button_hintbox', {
-              title : 'HintBox',
-              cmd : 'hintbox',
-              icon : 'wp_more',
-          });
+  if (tinymce !== undefined ) {
+    tinymce.create('tinymce.plugins.StonyRayHintBox', {
+        init : function(ed, url) {
+            ed.addButton('button_hintbox', {
+                title : 'HintBox',
+                cmd : 'hintbox',
+                icon : 'wp_more',
+            });
 
-          ed.addCommand('hintbox', function() {
-              var selected_text = ed.selection.getContent();
-              var return_text = '';
-              return_text = '<div class="hint-box">' + selected_text + '</div>';
-              ed.execCommand('mceInsertContent', 0, return_text);
-          });
-      },
-      createControl : function(n, cm) {
-        return null;
-      },
-  });
-  // Register plugin
-  tinymce.PluginManager.add( 'stonyray_hintbox', tinymce.plugins.StonyRayHintBox );
+            ed.addCommand('hintbox', function() {
+                var selected_text = ed.selection.getContent();
+                var return_text = '';
+                return_text = '<div class="hint-box">' + selected_text + '</div>';
+                ed.execCommand('mceInsertContent', 0, return_text);
+            });
+        },
+        createControl : function(n, cm) {
+          return null;
+        },
+    });
+    // Register plugin
+    tinymce.PluginManager.add( 'stonyray_hintbox', tinymce.plugins.StonyRayHintBox );
+  }
 })();
