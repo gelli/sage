167ff701d9287fe0b0307665b04ca0064bf0b612
gelli <cgellweiler@gmail.com>
2017-03-26 21:57:18 +0200

    Disable sidebar on sensei pages

--- lib/config.php
+++ lib/config.php
@@ -61,7 +61,8 @@ function display_sidebar() {
         'is_404',
         'is_front_page',
         ['is_page_template', 'template-custom.php'],
-        'is_woocommerce'
+        'is_woocommerce',
+        'is_sensei'
       ]
     );
 
